bb0a2bdea8dd650a5ea7b58085f13b806c70f5b6
PunkandClown <rasbwatri@gmail.com>
2020-12-01 19:23:45 +0500

    demo 1.2 fix

---
+++ src/main/java/com/bestSpringApplication/taskManager/model/task/classes/PatternImpl.java
@@ -0,0 +1,6 @@
+package com.bestSpringApplication.taskManager.model.task.classes;
+import com.bestSpringApplication.taskManager.model.task.interfaces.Pattern;
+
+public class PatternImpl implements Pattern {
+
+}
