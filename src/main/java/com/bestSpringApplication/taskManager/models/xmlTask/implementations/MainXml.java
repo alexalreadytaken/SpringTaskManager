663d2a894d269199378b9aa10b12ecc65b72ba23
Alex <nan@mail.com>
2020-12-15 17:54:19 +0500

    1.8.6 remove mainXml builder

--- src/main/java/com/bestSpringApplication/taskManager/models/xmlTask/implementations/MainXml.java
+++ src/main/java/com/bestSpringApplication/taskManager/models/xmlTask/implementations/MainXml.java
@@ -10,34 +10,37 @@ import java.util.Map;
 public class MainXml {
     private List<Task> tasks;
     private Map<String,String> taskFieldList;
-    private List<TaskDependencyImpl> taskDependencyList = new ArrayList<>();
+    private List<TaskDependencyImpl> taskDependencyList;
 
     public MainXml(){}
 
-    public static MainXmlBuilder startBuildXml(){
-        return new MainXml().new MainXmlBuilder();
+    public MainXml(List<Task> tasks, Map<String, String> taskFieldList, List<TaskDependencyImpl> taskDependencyList) {
+        this.tasks = tasks;
+        this.taskFieldList = taskFieldList;
+        this.taskDependencyList = taskDependencyList;
     }
-    public List<TaskDependencyImpl> getTaskDependencyList() { return taskDependencyList; }
-    public Map<String,String> getTaskFieldList() { return taskFieldList; }
-    public List<Task> getTasks() { return tasks; }
 
-    public class MainXmlBuilder {
+    public List<Task> getTasks() {
+        return tasks;
+    }
 
-        MainXmlBuilder(){}
-        public MainXmlBuilder setTasks(List<Task> tasks) {
-            MainXml.this.tasks = tasks;
-            return this;
-        }
-        public MainXmlBuilder setTaskFieldList(Map<String,String> taskFields){
-            MainXml.this.taskFieldList=taskFields;
-            return this;
-        }
-        public MainXmlBuilder setTaskDependencyList(List<TaskDependencyImpl> dependencies){
-            MainXml.this.taskDependencyList=dependencies;
-            return this;
-        }
-        public MainXml build(){
-            return MainXml.this;
-        }
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Map<String, String> getTaskFieldList() {
+        return taskFieldList;
+    }
+
+    public void setTaskFieldList(Map<String, String> taskFieldList) {
+        this.taskFieldList = taskFieldList;
+    }
+
+    public List<TaskDependencyImpl> getTaskDependencyList() {
+        return taskDependencyList;
+    }
+
+    public void setTaskDependencyList(List<TaskDependencyImpl> taskDependencyList) {
+        this.taskDependencyList = taskDependencyList;
     }
 }
