56e2c310b43f20fc61f0b1ec403676f0dcccde57
sanya <kekelofafamiliia@gmail.com>
2021-02-10 13:23:51 +0500

    2.4.4 removed IdRelation and ModifiableSingletonMap

--- src/main/java/com/bestSpringApplication/taskManager/repos/IdRelationRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bestSpringApplication.taskManager.repos;
-
-import com.bestSpringApplication.taskManager.models.idRelation.IdRelation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IdRelationRepo extends JpaRepository<IdRelation,Integer> {
-}
