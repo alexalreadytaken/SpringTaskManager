7fa742b71ae8357159ba299f3f001beb93869a97
ITIXYXYWA <ukv220@gmail.com>
2021-03-22 19:44:07 +0500

    2.1.0.1 add json and rewriting to export/import

--- src/main/resources/static/js/private/adminPage.js
+++
@@ -1,3 +0,0 @@
-import * as all from './helpers_Module.js'
-
-all.multiFetch('http://10.1.0.64:2000/schemas/master/Предмет_1')
---
+++ src/main/resources/static/js/private/main.js
@@ -0,0 +1,6 @@
+import * as all from './helpers_Module.js'
+import makeGraph from './making.js'
+
+// all.multiFetch('http://10.1.0.64:2000/schemas/master/Предмет_1')
+
+makeGraph()
