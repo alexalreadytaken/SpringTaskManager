4579a967132d59c90a47a16832dab6054ea9986f
ITIXYXYWA <ukv220@gmail.com>
2021-04-12 18:58:55 +0500

    2.4.2 more fixes

--- src/main/resources/static/js/private/making.js
+++ src/main/resources/static/js/private/making.js
@@ -5,52 +5,17 @@ import scheme2 from './local-JSON/scheme2.js';
 function makeGraph (arrData) {
     arrData = scheme1
 
-    let weakDepen = [], bigData = []
-
     console.log(arrData)
 
     const tasksPars = Object.entries(arrData.tasksMap).map(el => el[1])
     const depen = Object.entries(arrData.dependencies).map(el => el[1])
     tasksPars.splice(0,2)
     
-    console.log(tasksPars)
+    console.log(depen);
 
 // закидывем WEAK зависимости в отдельную переменную для удобной работы с ними
 
-    depen.forEach(el => {
-        if (el.relationType === 'WEAK') {
-            if ( el.id0.split('.')[1]) {
-                weakDepen.push({
-                    id0: el.id0.split('.')[1], id1: el.id1
-                })
-            } else {
-                weakDepen.push({
-                    id0: el.id0.split('.')[0], id1: el.id1
-                })
-            }
-        }
-    })
 
-// костыль для адекватного прокидывания в data.connectorTo (сделать через проверку повторяющихся выходящих тасков, чтобы они лежали в массиме и их было удобнее доставать из массива и передавать в data в str)
-
-    tasksPars.forEach( taskEl => {
-        weakDepen.forEach( weakEl => {
-            if (weakEl.id1 === taskEl.id) {
-                bigData.push({
-                    id1:weakEl.id1,
-                    id0: weakEl.id0
-                })
-            }
-        })
-    })
-
-    // tasksPars.forEach( el => {
-    //     bigData.forEach ( el1 => {
-    //         if (el.id = el1.id0) {
-    //             el.connectTo 
-    //         }
-    //     })
-    // })
 
     const data = tasksPars.filter(data => data.theme)
 
@@ -64,8 +29,6 @@ function makeGraph (arrData) {
     //     })
     //     return data 
     // }, [])
-
-    // console.log(data);
 
     data.forEach( el => {
         el.children = []
@@ -84,11 +47,8 @@ function makeGraph (arrData) {
 
     // id0 - Parent
     // id1 - Children
-    // нужно смотреть по зависимостям отцов, искать сразу всех детей
-    // лучше сделать через рекурсию -> чтобы функция смотрела есть ли у детей еще дети
 
     console.log(data)
-
     chartMaking(data)
 }
 
