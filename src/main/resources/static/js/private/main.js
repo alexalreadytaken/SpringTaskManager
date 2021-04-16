ca28aa88c853ce5a9a28a6606f736d7875ee262f
ITIXYXYWA <ukv220@gmail.com>
2021-04-16 23:55:52 +0500

    2.5.5.3 fix bug import

--- src/main/resources/static/js/private/main.js
+++ src/main/resources/static/js/private/main.js
@@ -1,5 +1,5 @@
 // import * as all from './helpers_Module.js'
-import multiFetch from './helpers_Module.js';
+import {multiFetch} from './helpers_Module.js';
 import makeGraph from './making.js';
 import scheme1 from './local-JSON/scheme1.js';
 
