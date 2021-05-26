2a2a2c0cc1906a9643001e26e2e788d5c3c269ff
ITIXYXYWA <ukv220@gmail.com>
2021-05-26 15:19:52 +0500

    2.7.9 finished send status and grade to server and refresh

--- src/static/js/private/interactionsTasks/setGrade.js
+++ src/static/js/private/interactionsTasks/setGrade.js
@@ -7,13 +7,12 @@ import { makeWeakDepen } from "../makeGraphGantt/parsingData/makeWeakDepen.js";
 import { parsDepen, parsTask } from "../makeGraphGantt/parsingData/parsFields.js";
 
 function setGrade(schemaId) {
-    // add HTML tags for universal form, so as not to delete everything all the time
+    let studentList
 
+    // add HTML tags to universal form, so as not to delete everything all the time
     document.getElementById('flex_Form').insertAdjacentHTML('afterbegin', `<div id = 'userId'><h1>Юзер</h1></div>`)
     document.getElementById('flex_Form').insertAdjacentHTML('beforeend', `<div id = 'statusCase'> <h1>Статус</h1> </div>`)
     document.getElementById('flex_Form').insertAdjacentHTML('beforeend', `<div id = 'gradeCase'><h1>Оценка</h1></div>`)
-
-    let studentList
 
 
     fetch(`http://10.3.0.87:2000/admin/schema/${schemaId}/task/${taskId}/state`).then(res => res.json())
@@ -25,7 +24,7 @@ function setGrade(schemaId) {
             `)
             
             document.getElementById('gradeCase').insertAdjacentHTML('beforeend', `
-                    <input id = 'grade${x}' placeholder = '2' type = 'number' min = '2' max = '5'/>
+                    <input id = 'grade${x}' value = '${el.grade}' type = 'number' min = '2' max = '5'/>
             `)
 
             status.forEach(el => {
@@ -33,9 +32,7 @@ function setGrade(schemaId) {
                         <option>${el}</option>
                 `)
             })
-        })
-
-        
+        })        
     })
 
     fetch('http://10.3.0.87:2000/admin/users').then(res => res.json())
@@ -49,35 +46,44 @@ function setGrade(schemaId) {
 
     document.getElementById('modalBtnSave').addEventListener('click', e => {
 
-
+        let tel = []
+            
         studentList.forEach((st, i) => {
-            const statusSelect = document.getElementById(`status${i}`).value
-            const gradeSelect = document.getElementById(`grade${i}`).value
-            console.log([statusSelect], [gradeSelect])
+
+            tel.push({
+                userId: st.userId,
+                grade: document.getElementById(`grade${i}`).value,
+                status: document.getElementById(`status${i}`).value
+            })
+
+
+            fetch(`http://10.3.0.87:2000/admin/user/${st.userId}/schema/${schemaId}/task/${taskId}?setGrade=${tel[i].grade}&setStatus=${tel[i].status}`).then(res => {
+                fetch(`http://10.3.0.87:2000/admin/schema/${schemaId}`).then(res => res.json())
+                .then(data => {
+                    let tasks = parsTask(data)
+    
+                    const thee = tasks.filter(el => el.theme)
+                    
+                    makeWeakDepen(tasks, parsDepen(data))
+                    
+                    makeChildren(thee, parsDepen(data), tasks)
+    
+                    getSummary({
+                        url: 'http://10.3.0.87:2000/admin/schema/1/summary',
+                        tasks: data
+                    }).then(res => {
+                        tasks = res            
+                        refreshChart(thee)
+                    })
+                })    
+            })
         })
 
-        fetch(`http://10.3.0.87:2000/admin/user/1/schema/${schemaId}/task/${taskId}?setGrade=5&setStatus=finished`)
-        
+        console.log(studentList)
 
+        console.log(tel)
+        tel = null
 
-        fetch(`http://10.3.0.87:2000/admin/schema/${schemaId}`).then(res => res.json())
-            .then(data => {
-                let tasks = parsTask(data)
-
-                const thee = tasks.filter(el => el.theme)
-                
-                makeWeakDepen(tasks, parsDepen(data))
-                
-                makeChildren(thee, parsDepen(data), tasks)
-
-                getSummary({
-                    url: 'http://10.3.0.87:2000/admin/schema/1/summary',
-                    tasks: data
-                }).then(res => {
-                    tasks = res            
-                    refreshChart(thee)
-                })
-            })
     })
 }
 
