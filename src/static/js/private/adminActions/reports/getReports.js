53963b4948749423f135306bd877469d66e2647d
ITIXYXYWA <ukv220@gmail.com>
2021-06-18 17:57:42 +0500

    2.7.16.2 add real student name to reports

--- src/static/js/private/adminActions/reports/getReports.js
+++ src/static/js/private/adminActions/reports/getReports.js
@@ -1,24 +1,29 @@
 import { config } from "../../config.js"
 
-
 function getReports () {
     let table = document.getElementById('tbody-reports')
     table.innerHTML = null
     fetch(`http://${config.url}/admin/tasks/unconfirmed`).then(response => response.json())
         .then(reports => {
-            reports.forEach(report => {
-                console.log(report)
-                table.insertAdjacentHTML('beforeend', `
-                    <tr>
-                        <td>${report.userId}</td>
-                        <td>${report.schemaId}</td>
-                        <td>${report.taskId}</td>
-                        <td>${report.percentComplete * 100}%</td>
-                        <td><button id = 'someId...'>проверить</button></td>
-                    </tr>
-                `)
+            fetch(`http://${config.url}/admin/users`).then(response => response.json())
+                .then(users => {
+                    users.forEach(user => {
+                        reports.forEach(report => {
+                            if (user.id === Number(report.userId)) {
+                                table.insertAdjacentHTML('beforeend', `
+                                    <tr>
+                                        <td>${user.name}</td>
+                                        <td>${report.schemaId}</td>
+                                        <td>${report.taskId}</td>
+                                        <td>${report.percentComplete * 100}%</td>
+                                        <td><button id = 'someId...'>проверить</button></td>
+                                    </tr>
+                                `)
+                            }
+                        })
+                    })
+                })
             })
-        })
 }
 
 
--- src/static/js/private/makeGraphGantt/main.js
+++ src/static/js/private/makeGraphGantt/main.js
@@ -1,7 +1,15 @@
 import { config } from '../config.js';
 import { makeGraph } from './parsingData/making.js';
 
+let SchemasPull = []
+
 // makeGraph(scheme1)
 fetch(`http://${config.url}/schema/1`)
     .then(response => response.json())
-    .then(response => makeGraph(response))
+    .then(response => SchemasPull.push(response))
+
+    setTimeout(() => {    
+        makeGraph(...SchemasPull)
+    }, 200);
+
+export {SchemasPull}
